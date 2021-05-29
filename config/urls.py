dc6649896c23650c9869cad9eba56627cc92111d
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-05-29 18:03:00 +0300

    ViewSet for Poll created

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from polls.views import home_page
 urlpatterns = [
-    path('', home_page, name='home-page'),
     path('admin/', admin.site.urls, name='admin-panel'),
-    re_path(r'^api/', include('polls.urls')),
+    re_path(r'^', include('polls.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -7,12 +8,7 @@ from .models import Poll, Choice
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
-class PollList(generics.ListCreateAPIView):
-    queryset = Poll.objects.all()
-    serializer_class = PollSerializer
-
-
-class PollDetail(generics.RetrieveDestroyAPIView):
+class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
