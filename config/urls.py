d1f65d9c0830366153ce035d2c3b5ec312358b41
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-05-29 17:29:18 +0300

    apiviews without ViewSets

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from polls.views import home_page
-
 urlpatterns = [
     path('', home_page, name='home-page'),
     path('admin/', admin.site.urls, name='admin-panel'),
-    path('api/', include('polls.urls')),
+    re_path(r'^api/', include('polls.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Poll, Choice, Vote
-
+from .models import Poll, Choice
 
 admin.site.register(Poll)
 admin.site.register(Choice)
-admin.site.register(Vote)
