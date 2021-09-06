c93d72832278396dc906ffa4fed6d287a579045d
Flantropy <80071496+flantropy@users.noreply.github.com>
2021-09-06 06:56:24 +0300

    Comments for models added. Init comments removed

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Poll(models.Model):
+    """
+    Poll is creating by superuser, serves as root node for other models
+    and can have any number of questions.
+    """
     title = models.CharField(max_length=100, unique=True, default='Poll Title')
     description = models.CharField(max_length=500, default='Describe your poll here')
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
@@ -14,6 +18,11 @@ class Poll(models.Model):
 
 
 class Question(models.Model):
+
+    """
+    Question is creating by superuser, it's bound to particular poll,
+    can have any number of answers and sets type of expected answer.
+    """
     poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
     question_text = models.CharField(max_length=500, default='Propose some question')
 
@@ -29,6 +38,11 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+    """
+    Answer is creating by superuser, and serves as variant for it's question.
+    If question expects answer of type AnswerType.TEXT, 'answer_text' field should be
+    set to 'user input'.
+    """
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=300, default='')
 
@@ -36,13 +50,13 @@ class Answer(models.Model):
         return self.answer_text
 
 
-# TODO  resolve unique user id problem
-# noinspection PyCallingNonCallable
 class Vote(models.Model):
+    """
+    Vote can store key of answer voted by user, or 'user input'
+    depending on answer type expected for question.
+    """
     answer = models.ForeignKey(Answer, related_name='votes', editable=False,
                                on_delete=models.CASCADE)
-    # voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    # question = models.ForeignKey(Question, related_name='votes', on_delete=models.CASCADE)
     user = models.CharField(max_length=50, editable=False)
     user_input = models.CharField(max_length=300, default='')
 
